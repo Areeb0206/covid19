40c3333a50c14da7935e314a8f5d4048dfe37103
Areeb0206 <masafvi48@gmail.com>
2021-03-17 21:02:14 +0530

    first commit

---
+++ js/contact.js
@@ -0,0 +1,20 @@
+$(".menu-activator").on("click", function() {
+  $("body").toggleClass("menu-active");
+});
+
+
+
+
+
+
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+
+
+
+
+
